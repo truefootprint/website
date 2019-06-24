6e2a94ace03023612b9dbae714cfeb31795b6c40
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-24 18:00:31 +0100

    Add the Hero text

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -2,11 +2,24 @@ import css from "./styles.scss";
 
 const Hero = () => (
   <div className={css.hero}>
-    <video autoPlay loop>
-      <source src="/static/cranes.mp4" type="video/mp4" />
-    </video>
+    <div className={css.background}>
+      <video autoPlay loop>
+        <source src="/static/cranes.mp4" type="video/mp4" />
+      </video>
 
-    <div className={css.crosshatch} />
+      <div className={css.crosshatch} />
+    </div>
+
+    <div className={css.foreground}>
+      <h3 className={css.text}>
+        <div>We help companies</div>
+        <div>increase the</div>
+        <div>return on investment</div>
+        <div>of their</div>
+        <div>sustainability</div>
+        <div>projects</div>
+      </h3>
+    </div>
   </div>
 );
 
