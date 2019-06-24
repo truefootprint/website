c057bf6bbc5f5230031e9fcb55b7180addebeb31
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-24 18:01:54 +0100

    Add the Logo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
 import Layout from "../components/layout";
 import Hero from "../components/hero";
+import Logo from "../components/logo";
 
 const Index = () => (
   <Layout>
+    <Logo />
     <Hero />
   </Layout>
 );
