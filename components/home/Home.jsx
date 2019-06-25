4dc3b15b54fed03278f821426e8c9ec5e2a019c7
Chris Patuzzo <chris@patuzzo.co.uk>
2019-06-25 12:43:16 +0100

    Add the Solution section to the homepage

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -10,45 +10,71 @@ const Home = () => (
     <NavBar />
     <Hero />
 
-    <section className={css.section}>
-      <Heading>Scorecards</Heading>
+    <div className={css.content}>
+      <section className={css.side_by_side}>
+        <div className={css.left}>
+          <Heading>Solution</Heading>
 
-      <p>
-        We have analysed sustainability and annual reports in key industries.
-        We have found that they do not report enough on the <b>outcomes</b> of sustainability efforts.
-        Our aim is to help companies be strategic and impactful in their reporting on sustainability.
-      </p>
+          <p>
+          We work with the beneficiaries of your sustainability projects:
+          the residents of the rural mining villages where you are constructing roads;
+          the cocoa farmers whose children go to the new schools you’re paying for.
+          </p>
 
-      <p>
-        You can download our free reports:
-      </p>
+          <p>
+          Our technology empowers them to collect data and to take ownership of
+          solutions to improve their lives.
+          </p>
 
-      <ul>
-        <li>
-          Agribusiness: <a href={url("/static/01-agribusiness-sustainability-report-2019.pdf")} target="_blank">
-            The State of Agribusiness Sustainability 2019</a>
-        </li>
+          <p>
+          The people at the base of your supply chain deliver the data you need
+          because it works for them.  It’s good for them and it’s good for you.
+          </p>
+        </div>
 
-        <li>
-          Cocoa, Coffee, Tea: <a href={url("/static/02-cocoa-coffee-tea-sustainability-report-2019.pdf")} target="_blank">
-            The State of Cocoa, Coffee, Tea Sustainability 2019</a>
-        </li>
+        <div className={css.right} />
+      </section>
 
-        <li>Mining: <a href={url("/static/03-mining-sustainability-report-2019.pdf")} target="_blank">
-          The State of Mining Sustainability 2019</a>
-        </li>
-      </ul>
-    </section>
+      <section className={css.full_width}>
+        <Heading>Scorecards</Heading>
 
-    <section className={css.section}>
-      <Heading>Contact</Heading>
+        <p>
+          We have analysed sustainability and annual reports in key industries.
+          We have found that they do not report enough on the <b>outcomes</b> of sustainability efforts.
+          Our aim is to help companies be strategic and impactful in their reporting on sustainability.
+        </p>
 
-      <h4>Inquiries</h4>
+        <p>
+          You can download our free reports:
+        </p>
 
-      <p>
-        For any inquiries, questions or commendations, please email us at <a href="mailto:info@truefootprint.com">info@truefootprint.com</a>.
-      </p>
-    </section>
+        <ul>
+          <li>
+            Agribusiness: <a href={url("/static/01-agribusiness-sustainability-report-2019.pdf")} target="_blank">
+              The State of Agribusiness Sustainability 2019</a>
+          </li>
+
+          <li>
+            Cocoa, Coffee, Tea: <a href={url("/static/02-cocoa-coffee-tea-sustainability-report-2019.pdf")} target="_blank">
+              The State of Cocoa, Coffee, Tea Sustainability 2019</a>
+          </li>
+
+          <li>Mining: <a href={url("/static/03-mining-sustainability-report-2019.pdf")} target="_blank">
+            The State of Mining Sustainability 2019</a>
+          </li>
+        </ul>
+      </section>
+
+      <section className={css.full_width}>
+        <Heading>Contact</Heading>
+
+        <h4>Inquiries</h4>
+
+        <p>
+          For any inquiries, questions or commendations, please email us at <a href="mailto:info@truefootprint.com">info@truefootprint.com</a>.
+        </p>
+      </section>
+    </div>
   </Layout>
 );
 
