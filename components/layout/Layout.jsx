f180f84ac602ef32bd1788332310e4b7d1dc82dc
Chris Patuzzo <chris@patuzzo.co.uk>
2019-07-19 11:49:43 +0100

    Add a favicon

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -5,6 +5,7 @@ import "./typography.scss";
 
 const Layout = ({ children }) => <>
   <Head>
+    <link rel="shortcut icon" href="/static/favicon.png" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/89bcaf21e8.js"></script>
   </Head>
