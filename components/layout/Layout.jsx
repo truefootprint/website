51cf76421f38418df59bc41ffc6cfe1eb169ddda
Chris Patuzzo <chris@patuzzo.co.uk>
2019-07-19 11:51:14 +0100

    Set the title

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -8,6 +8,7 @@ const Layout = ({ children }) => <>
     <link rel="shortcut icon" href="/static/favicon.png" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet" />
     <script src="https://kit.fontawesome.com/89bcaf21e8.js"></script>
+    <title>TrueFootprint</title>
   </Head>
 
   {children}
