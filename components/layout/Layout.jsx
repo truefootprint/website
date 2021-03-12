3f87ccc901d05ff670595e92ff070a53aac80fa3
Chanda Pwapwa <mulaikopwapwa@gmail.com>
2021-03-12 10:45:03 +0200

    website changes

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -11,7 +11,7 @@ ReactGA.pageview("/");
 
 const Layout = ({ children }) => <>
   <Head>
-    <link rel="shortcut icon" href="/static/favicon.png" />
+    <link rel="shortcut icon" href="/static/TF-Favicon.svg" />
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900" rel="stylesheet" />
     <title>TrueFootprint</title>
   </Head>
